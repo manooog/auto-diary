4772aef7ac58750be00aed49b8fd08b09539080c
阮细浩 <ruanxihao@fcbox.com>
2019-07-15 11:02:09 +0800

    feat: 支持后期切换远端服务的地址

--- src/extension.ts
+++ src/extension.ts
@@ -1,67 +1,73 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { cmd } from './cmd';
-import { infoStatusBar } from './util';
-import { pullRebaseDiary, readConfig, checkDirAndInit } from './main';
-var dayjs = require('dayjs');
+import * as vscode from 'vscode'
+import { cmd } from './cmd'
+import { infoStatusBar } from './util'
+import { pullRebaseDiary, readConfig, checkDirAndInit } from './main'
+var dayjs = require('dayjs')
 
 export interface ExtStatus {
-	synced: boolean;
-	config: ExtConfig;
+  synced: boolean
+  config: ExtConfig
 }
 
 export interface ExtConfig {
-	branch: string;
-	remote: string;
+  branch: string
+  remote: string
 }
 
 export let extStatus: ExtStatus = {
-	synced: false,
-	config: {
-		branch: '',
-		remote: ''
-	}
-};
+  synced: false,
+  config: {
+    branch: '',
+    remote: '',
+  },
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
+  extStatus.config = await readConfig()
 
-	extStatus.config = await readConfig();
-	 
-	 
-	 
-	let disposable = vscode.commands.registerCommand('extension.syncRemote', async () => {
-		extStatus.synced = await pullRebaseDiary();
-	});
-		
-	await checkDirAndInit();
-	extStatus.synced = await pullRebaseDiary();
+  let disposable = vscode.commands.registerCommand(
+    'extension.syncRemote',
+    async () => {
+      extStatus.synced = await pullRebaseDiary()
+    }
+  )
 
-	vscode.workspace.onDidSaveTextDocument(async (e) => {
-		
-		try {
-			await cmd(`git add .`);
+  await checkDirAndInit()
+  extStatus.synced = await pullRebaseDiary()
 
-			await cmd(`git commit -m commit_by_vsc-auto-diary-ext_${dayjs().format('YYYY/MM/DD_HH:mm')}`);
-			
-			if(!extStatus.synced) {
-				// init sync faild, avoid comflict, stop auto push!
-				vscode.window.showWarningMessage('[auto-diary]: try reload window to sync remote again!');
-			} else {
-				await cmd('git push').then(() => {
-					infoStatusBar('sync done!');
-				});
-			}
+  vscode.workspace.onDidSaveTextDocument(async e => {
+    try {
+      await cmd(`git add .`)
 
-		} catch (error) {
-			vscode.window.showErrorMessage('[auto-diary]:', error);
-		}
+      await cmd(
+        `git commit -m commit_by_vsc-auto-diary-ext_${dayjs().format(
+          'YYYY/MM/DD_HH:mm'
+        )}`
+      )
 
-	});
-	
-	context.subscriptions.push(disposable);
+      if (!extStatus.synced) {
+        // init sync faild, avoid comflict, stop auto push!
+        vscode.window.showWarningMessage(
+          '[auto-diary]: try reload window to sync remote again!'
+        )
+      } else {
+        infoStatusBar('pushing!')
+        await cmd(`git push auto-diary HEAD:${extStatus.config.branch}`).then(
+          () => {
+            infoStatusBar('sync done!')
+          }
+        )
+      }
+    } catch (error) {
+      vscode.window.showErrorMessage('[auto-diary]:', error)
+    }
+  })
+
+  context.subscriptions.push(disposable)
 }
 
 // this method is called when your extension is deactivated
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,54 @@
-import { cmd } from "./cmd";
-import { infoStatusBar } from "./util";
-import { readFile } from 'fs';
-import * as vscode from 'vscode';
-import { ExtConfig, extStatus } from "./extension";
+import { cmd } from './cmd'
+import { infoStatusBar } from './util'
+import { readFile } from 'fs'
+import * as vscode from 'vscode'
+import { ExtConfig, extStatus } from './extension'
 
 export async function pullRebaseDiary() {
-    try {
-        await cmd('git pull --rebase').then(() => {
-            infoStatusBar('sync success!');
-        });
-        return true;
-    } catch (error) {
-        infoStatusBar('sync faild!');
-        return false;
-    }
+  infoStatusBar('syncing!')
+  try {
+    await cmd(`git pull auto-diary ${extStatus.config.remote} --rebase`).then(
+      () => {
+        infoStatusBar('sync success!')
+      }
+    )
+    return true
+  } catch (error) {
+    infoStatusBar('sync faild!')
+    return false
+  }
 }
 
 export async function checkDirAndInit() {
-    try {
-        await cmd('git status');
-    } catch (error) {
-        await cmd(`git init && git remote add auto-diary ${extStatus.config.remote}`);
-        //set track
-        // watch out !! will reset workdir!!
-        await cmd(`git fetch auto-diary && git checkout ${extStatus.config.branch} -f`);
-    }
+  //TODO: 增加远程服务的指纹
+  //ssh-keyscan code.aliyun.com >> ~/.ssh/known_hosts
+  try {
+    await cmd('git status')
+  } catch (error) {
+    cmd('git init')
+  }
+  if (await cmd(`git remote -v | grep \auto-diary`)) {
+    await cmd(`git remote set-url auto-diary ${extStatus.config.remote}`)
+  } else {
+    await cmd(`git remote add auto-diary ${extStatus.config.remote}`)
+  }
+  //set track
+  // watch out !! will reset workdir!!
+  await cmd(
+    `git fetch auto-diary && git checkout auto-diary/${
+      extStatus.config.branch
+    } -f`
+  )
 }
 
 export function readConfig(): Promise<ExtConfig> {
-    return new Promise((resolve, reject) => {
-        readFile(`${vscode.workspace.rootPath}/.auto-diary.json`, (err, data) => {
-            if(err) {
-                reject(err);
-            } else {
-                resolve(JSON.parse(data.toString()));
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    readFile(`${vscode.workspace.rootPath}/.auto-diary.json`, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(JSON.parse(data.toString()))
+      }
+    })
+  })
 }
